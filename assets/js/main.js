2fb0353086290356f0425b685730d990aa8a1fb6
ThiagoxMedeiros <thiagoxmedeiros@gmail.com>
2024-11-21 13:03:28 -0300

    Salvando as primeiras modificacoes no codigo: a criacao de uma nova pagina para armazenar os detalhes dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
 const pokemonList = document.getElementById('pokemonList')
+const pokemonsDetails = document.getElementById('pokemonDetails')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+
+function convertPokemonToPokemonDetail(pokemon) {
+    return `
+        <div class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    </div>
+    `
+}
+
 function convertPokemonToLi(pokemon) {
     return `
+     <a href="./pokemon-detail.html">
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
@@ -20,7 +42,15 @@ function convertPokemonToLi(pokemon) {
                      alt="${pokemon.name}">
             </div>
         </li>
+    </a>
     `
+}
+
+function loadPokemonDetails(Pokemon) {
+    pokeApi.getPokemons(offset).then((pokemons = []) => {
+        const newHtml = pokemons.map(onvertPokemonToPokemonDetail).join('')
+        pokemonsDetails.innerHTML += newHtml
+    })
 }
 
 function loadPokemonItens(offset, limit) {
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,6 +5,10 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+    pokemon.stats = pokeDetail.stats
+    
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
